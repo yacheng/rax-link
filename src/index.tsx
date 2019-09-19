2f55682dfa11ea249e30428927e699a77a276b01
Mega <761647716@qq.com>
2019-09-19 14:18:22 +0800

    refactor: remove third-party library

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,33 @@
-import {
-  createElement,
-  useRef,
-  forwardRef,
-  useImperativeHandle,
-  ForwardRefExoticComponent
-} from 'rax';
-import Text from 'rax-text';
-import omit from 'omit.js';
-import { LinkProps } from './types';
+import { createElement, forwardRef, ForwardRefExoticComponent } from "rax";
+import Text from "rax-text";
+import { LinkProps } from "./types";
 
 const Link: ForwardRefExoticComponent<LinkProps> = forwardRef((props, ref) => {
-  const linkRef = useRef<HTMLLinkElement>(null);
-  const { style = {}, children } = props;
-  const nativeProps = { ...props };
+  const { className, style = {}, onClick, onPress, children, ...rest } = props;
   const textStyle = {
     color: style.color,
     lines: style.lines,
     fontSize: style.fontSize,
     fontStyle: style.fontStyle,
     fontWeight: style.fontWeight,
-    textDecoration: style.textDecoration || 'none',
+    textDecoration: style.textDecoration || "none",
     textAlign: style.textAlign,
     fontFamily: style.fontFamily,
     textOverflow: style.textOverflow
   };
-
-  useImperativeHandle(ref, () => linkRef.current);
-
-  if (nativeProps.onPress) {
-    nativeProps.onClick = nativeProps.onPress;
-  }
-
   return (
     <a
-      ref={linkRef}
+      {...rest}
+      ref={ref}
+      className={className}
       style={style}
-      {...omit(nativeProps, ['style', 'onPress', 'children'])}
+      onClick={onClick || onPress}
     >
-      {typeof children === 'string' ? (
+      {typeof children === "string" ? (
         <Text style={textStyle}>{children}</Text>
-      ) :
+      ) : (
         children
-      }
+      )}
     </a>
   );
 });
